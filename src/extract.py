0425c1222f387fd064daad794517f4b6a22dacec
Fábio Santos <fabio.sildossantos@gmail.com>
2026-01-14 11:31:03 -0300

    .

--- src/extract.py
+++ src/extract.py
@@ -1,6 +1,10 @@
 import pandas as pd
+from pathlib import Path
 
-def extract_data(path="/data/vendas.csv"):
-    df = pd.read_csv(path)
+BASE_DIR = Path(__file__).resolve().parent.parent  # raiz do projeto
+
+def extract_data(path="data/vendas.csv"):
+    full_path = BASE_DIR / path
+    df = pd.read_csv(full_path)
     df["data_venda"] = pd.to_datetime(df["data_venda"])
     return df
--- src/load.py
+++ src/load.py
@@ -1,16 +1,19 @@
-import os
+from pathlib import Path
 
-def load_data(vendas_por_periodo, produtos_por_periodo, output_dir="/output"):
-    os.makedirs(output_dir, exist_ok=True)
+BASE_DIR = Path(__file__).resolve().parent.parent  # raiz do projeto
+
+def load_data(vendas_por_periodo, produtos_por_periodo, output_dir="output"):
+    output_path = BASE_DIR / output_dir
+    output_path.mkdir(exist_ok=True)
 
     vendas_por_periodo.to_csv(
-        f"{output_dir}/vendas_por_periodo_mes.csv",
+        output_path / "vendas_por_periodo_mes.csv",
         index=False,
         encoding="utf-8-sig"
     )
 
     produtos_por_periodo.to_csv(
-        f"{output_dir}/produtos_por_periodo_mes.csv",
+        output_path / "produtos_por_periodo_mes.csv",
         index=False,
         encoding="utf-8-sig"
     )
